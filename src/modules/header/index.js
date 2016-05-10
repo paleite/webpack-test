e664014b26b984b052f0b0d46717150b76d637e6
Patrick Aleite <paleite@kth.se>
2016-05-10 10:51:47 +0200

    Added missing debug-message

--- src/modules/header/index.js
+++ src/modules/header/index.js
@@ -2,6 +2,7 @@ module.exports = function () {
   require('./header.scss')
   console.log('Loaded SCSS header.scss')
   require('modules/nav')()
-  require('modules/search')()
   console.log('Loaded module nav')
+  require('modules/search')()
+  console.log('Loaded module search')
 }
