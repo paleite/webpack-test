6ab753bcd672b60728c67f9f07e54f2b81abf411
Patrick Aleite <paleite@kth.se>
2016-05-11 17:33:33 +0200

    Added test-image and updated webpack-config

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -19,6 +19,14 @@ $('body').addClass('loading') // TODO: Dev only, remove in real project
 require('modules/header')()
 console.log('Loaded module header')
 
+// var responsive = JSON.parse(require('./test-image.png'))
+
+// $('#webpack').text(JSON.stringify(responsive))
+// $('img[data-image="' + 'test-image.png' + '"]').attr('src', responsive.placeholder).attr('srcset', responsive.srcset)
+
+const testImage = require('!!url!./test-image.png') // Override loaders and force inclusion as url
+$('img[data-image="' + 'test-image.png' + '"]').attr('src', testImage)
+
 console.log('Finished loading content above the fold')
 
 require.ensure([
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
 'use strict'
 const webpack = require('webpack')
-const path = require('path')
-const commonsPlugin = new webpack.optimize.CommonsChunkPlugin({
-  name: 'common',
-  minChunks: 2
-})
+// const commonsPlugin = new webpack.optimize.CommonsChunkPlugin({
+//   name: 'common',
+//   minChunks: 2
+// })
+const commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common', 'common.bundle.js', Infinity)
 const providePlugin = new webpack.ProvidePlugin({
   $: 'jquery',
-  jQuery: 'jquery'
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery'
 })
 // const SassLintPlugin = require('sasslint-webpack-plugin')
 const autoprefixer = require('autoprefixer')
-const svgoConfig = JSON.stringify({
-  plugins: [
-  // TODO: Configure this plugin
-  {removeTitle: true},
-  {convertColors: {shorthex: false}},
-  {convertPathData: false}
-  ]
-})
+// const svgoConfig = JSON.stringify({
+//   plugins: [
+//   // TODO: Configure this plugin
+//   {removeTitle: true},
+//   {convertColors: {shorthex: false}},
+//   {convertPathData: false}
+//   ]
+// })
 const merge = require('webpack-merge')
 const parts = require('./lib/parts')
 let config
 
 const common = {
-  devtool: 'eval',
-
-  // Step 1: Source Maps
-  // devtool: 'cheap-module-source-map',
+  // devtool: 'eval',
 
   context: __dirname,
   entry: {
+    common: ['bootstrap-loader'],
+    index: './src/pages/index',
     'blog-post': './src/pages/blog-post',
     'blog-post-list': './src/pages/blog-post-list',
-    'event-list': './src/pages/event-list',
-    'job-list': './src/pages/job-list',
-    'search-results': './src/pages/search-results',
-    common: ['bootstrap-loader'],
     event: './src/pages/event',
-    index: './src/pages/index',
-    job: './src/pages/job'
+    'event-list': './src/pages/event-list',
+    job: './src/pages/job',
+    'job-list': './src/pages/job-list',
+    'search-results': './src/pages/search-results'
   },
   output: {
     path: 'dist',
@@ -48,21 +46,12 @@ const common = {
     chunkFilename: '[name].[chunkhash].chunk.js'
   },
 
-  // Step 2: Node environment
-  // plugins: [
-  //   new webpack.DefinePlugin({
-  //     'process.env': {
-  //       'NODE_ENV': JSON.stringify('production')
-  //     }
-  //   })
-  // ],
-
   plugins: [
-    commonsPlugin,
+    providePlugin,
+    commonsPlugin
     // new SassLintPlugin({
     //   glob: 'src/**/*.scss'
     // }),
-    providePlugin
   ],
 
   module: {
@@ -75,7 +64,22 @@ const common = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel?presets[]=es2015'
+        loader: 'babel?presets[]=es2015-webpack'
+      },
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/i,
+      //   loaders: [
+      //     // 'file?hash=sha512&digest=hex&name=[hash].[ext]',
+      //     'url?limit=25000',
+      //     'resize-image?sizes[]=480w,sizes[]=768w,sizes[]=992w,sizes[]=1200w&placeholder'
+      //   ]
+      // },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          // 'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'url?limit=10000'
+        ]
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -84,15 +88,32 @@ const common = {
       {
         test: /\.(ttf|eot)(\?[\s\S]+)?$/,
         loader: 'file'
+      }
+      // ,
+      // {
+      //   test: /.*\.svg$/,
+      //   loaders: [
+      //     'file',
+      //     'svgo?' + svgoConfig
+      //   ]
+      // }
+    ],
+    imageWebpackLoader: {
+      pngquant: {
+        quality: '65-90',
+        speed: 4
       },
-      {
-        test: /.*\.svg$/,
-        loaders: [
-          'file',
-          'svgo?' + svgoConfig
+      svgo: {
+        plugins: [
+          {
+            removeViewBox: false
+          },
+          {
+            removeEmptyAttrs: false
+          }
         ]
       }
-    ]
+    }
   },
   resolve: {
     extensions: ['', '.js', '.scss'],
@@ -105,7 +126,40 @@ const common = {
 
 switch (process.env.npm_lifecycle_event) {
   case 'build':
-    config = merge(common, {})
+    config = merge(
+      common,
+      {
+        // devtool: 'cheap-module-source-map',
+        plugins: [
+          new webpack.optimize.UglifyJsPlugin({
+            compress: {
+              // booleans: true,
+              // conditionals: true,
+              dead_code: true,
+              // drop_debugger: true,
+              drop_console: true,
+              // if_return: true,
+              // join_vars: true,
+              screw_ie8: true,
+              // sequences: true,
+              // unused: true,
+              warnings: false
+            },
+            comments: false,
+            mangle: {
+              // props: /matching_props/,
+              except: ['$super', '$', 'exports', 'require']
+            },
+            output: {
+              comments: false
+            },
+            sourceMap: false
+          }),
+          new webpack.optimize.OccurrenceOrderPlugin(),
+          new webpack.optimize.DedupePlugin()
+        ]
+      }
+    )
     break
   default:
     // Dev
